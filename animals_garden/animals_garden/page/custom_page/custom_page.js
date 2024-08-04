d8e0d6658648ce9eacbdff295cea640beacae97d
khalid <iq2lfl@gmail.com>
2024-08-04 18:50:23 +0300

    html content test in page

--- animals_garden/animals_garden/page/custom_page/custom_page.js
+++ animals_garden/animals_garden/page/custom_page/custom_page.js
@@ -1,7 +1,40 @@
+
 frappe.pages['custom-page'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'custom page',
-		single_column: true
-	});
+	new PageContent(wrapper); // just initialize it
+
+
 }
+
+
+PageContent = Class.extend({
+	init: function(wrapper){
+		 this.page = frappe.ui.make_app_page({ //             make var this. , in py self , to recognize it 
+			parent: wrapper,
+			title: 'custom page',
+			single_column: true
+		}); //               this func must be here instead of above this more better place
+
+		this.make(); //       this trigger the above function with the below make func
+
+	}, //                takes the whole page
+
+
+	make: function() {
+		// html content - css - js
+		
+		let htmlContent = `
+
+		<h2> This is our page </h2>
+		<h3> This is our page </h3>
+		<h4> This is our page </h4>
+
+
+
+
+		`;
+
+
+		$(frappe.render_template( htmlContent, this)).appendTo(this.page.main);  
+	
+	}
+})
